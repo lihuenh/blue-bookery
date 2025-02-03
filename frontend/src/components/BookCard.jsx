69ad1b55736dbe8b530ae4d5ade283133b55d180
Lihuen Heredia <lihuenherediam@gmail.com>
2025-02-03 16:35:36 -0300

    Cambios grid en books list, tamano tarjeta.

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -4,15 +4,12 @@ import { Link } from 'react-router-dom'
 function BookCard({ book, onDelete, darkMode }) {
   return (
     <div
-      className={`rounded-lg shadow-lg overflow-hidden transition-all duration-400 ${
-        darkMode ? 'bg-gray-700 ' : 'bg-gray-100'
-      }`}
+      className='flex flex-col h-[500px] rounded-lg shadow-lg overflow-hidden transition-all duration-400 
+      bg-gray-100 text-gray-700 dark:bg-gray-700 dark:text-gray-200'
     >
-      <Link
-        to={`/edit-book/${book.id}`} // Redirigir a la página de edición
-      >
+      <Link to={`/edit-book/${book.id}`} className='h-[60%]'>
         <img
-          className='rounded-t-lg object-cover w-full h-48'
+          className='h-full w-full object-cover'
           src={
             book.image && book.image !== ''
               ? book.image
@@ -21,30 +18,25 @@ function BookCard({ book, onDelete, darkMode }) {
           alt={book.title}
         />
       </Link>
-      <div className={`p-4 ${darkMode ? 'text-gray-200' : 'text-gray-700'}`}>
-        <h3 className='text-xl font-semibold'>{book.title}</h3>
-        <p className={`${darkMode ? 'text-gray-400' : 'text-gray-700'}`}>
-          by {book.author}
-        </p>
-        <p className={`${darkMode ? 'text-gray-400' : 'text-gray-700'}`}>
-          Year: {book.year}
-        </p>
 
-        <div className='mt-4 flex justify-between'>
+      <div className='h-[40%] p-4 flex flex-col justify-between'>
+        <div>
+          <h3 className='text-lg font-semibold'>{book.title}</h3>
+          <p className='text-sm'>{book.author}</p>
+          <p className='text-sm'>Year: {book.year}</p>
+        </div>
+
+        <div className='flex justify-between'>
           <Link
-            to={`/book/${book.id}`} // Asegúrate de que 'book.id' esté correcto
-            className={`${
-              darkMode ? 'bg-blue-600' : 'bg-blue-500'
-            } text-white p-2 rounded hover:bg-blue-600`}
+            to={`/book/${book.id}`}
+            className='bg-blue-500 text-white p-2 rounded hover:bg-blue-600'
           >
             View Details
           </Link>
 
           <button
-            onClick={() => onDelete(book.id)} // Asegúrate de que `onDelete` está correctamente pasado como prop
-            className={`${
-              darkMode ? 'bg-red-600' : 'bg-red-500'
-            } text-white p-2 rounded hover:bg-red-600`}
+            onClick={() => onDelete(book.id)}
+            className='bg-red-500 text-white p-2 rounded hover:bg-red-600'
           >
             Delete
           </button>
--- frontend/src/components/BookList.jsx
+++ frontend/src/components/BookList.jsx
@@ -3,7 +3,7 @@ import BookCard from './BookCard'
 
 function BookList({ books, onDelete, onEdit, darkMode }) {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-6 gap-4 mt-2'>
+    <div className='grid grid-cols-auto-fit gap-6 justify-center mt-2'>
       {books.map((book) => (
         <BookCard
           key={book.id}
